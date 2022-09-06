4a66781189941615d78dd3263cd8eff5fa6dff12
Kuvshinov Vlad <kuvshinov_v@mail.ru>
2022-09-06 21:38:55 +0300

    Добавил решение второй задачи

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
-
+/* 
  void ShowArray(int[] array){
     int count = 0;
     Console.Write($"[ ");
@@ -33,7 +33,7 @@ while ( i <= size-1 )        // Использовал while т.к. началь
     i++;
 }
 ShowArray(mass);
-
+ */
 
 
 
@@ -45,3 +45,17 @@ ShowArray(mass);
 //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+// сначала приравниваем обе части равенств 
+//k1 * x + b1 = k2 * x + b2    ->    и находим х
+double x = (b2-b1)/(k1-k2);
+double y = k1*x+b1;
+Console.WriteLine($"Точка пересечения двух прямых будет в координатах (х={x}), (y={y})");
